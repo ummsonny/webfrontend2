1295c3beab8d1dc66a5ae09c9c46333426f749fe
ummsonny <78682163+ummsonny@users.noreply.github.com>
2025-08-19 14:30:21 +0900

    2-3
    useQuery로 영화목록 캐싱하기

--- src/components/Delayed.tsx
+++ src/components/Delayed.tsx
@@ -15,5 +15,13 @@ export default function Delayed({ wait }: { wait?: number }) {
       <h2>{JSON.stringify(data)}</h2>
       <h3>{JSON.stringify(isLoading)}</h3>
     </>
+
+    // isFetching은 항상, isLoading은 처음 한번만 가져올때 true가 된다.(isPending은 잘 안씀)
+    //     <>
+    //   <div>isFetching: {JSON.stringify(isFetching)}</div>
+    //   <div>isPending: {JSON.stringify(isPending)}</div>
+    //   <div>isLoading: {JSON.stringify(isLoading)}</div>
+    //   <div>{data?.time}</div>
+    // </>
   )
 }
